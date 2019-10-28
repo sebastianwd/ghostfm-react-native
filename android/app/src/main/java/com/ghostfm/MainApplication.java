7a9354c5472ac3ffbeb151e98c6b85739901cdfa
Sebastian <sebastian.luque.pl@gmail.com>
2019-10-28 17:21:54 -0500

    first

--- android/app/src/main/java/com/ghostfm/MainApplication.java
+++ android/app/src/main/java/com/ghostfm/MainApplication.java
@@ -1,9 +1,13 @@
 package com.ghostfm;
 
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
+import com.cinder92.musicfiles.RNReactNativeGetMusicFilesPackage;
+import com.drazail.rnandroidstore.RNAndroidStorePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new RNAndroidStorePackage());
           return packages;
         }
 
